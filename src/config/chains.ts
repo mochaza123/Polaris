8a0ddfb6c99be8a10bfb24086ea1a62acd606d78
mochaza123 <58312722+mochaza123@users.noreply.github.com>
2023-12-08 17:15:13 +0700

    Update chains.ts

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,61 +1,33 @@
 import { defineChain } from "viem";
 import {
-  arbitrum,
-  avalanche,
-  base,
-  bsc,
-  celo,
-  classic,
-  confluxESpace,
-  fantom,
-  gnosis,
-  linea,
-  mainnet,
-  okc,
-  opBNB,
-  polygon,
-  zkSync,
+  doge,
 } from "viem/chains";
 
-export const shibarium = defineChain({
+export const doge = defineChain({
   id: 109,
-  name: "Shibarium",
-  network: "shibarium",
+  name: "dogechain",
+  network: "doge",
   nativeCurrency: {
-    decimals: 18,
-    name: "BONE",
-    symbol: "BONE",
+    decimals: 2000,
+    name: "doge",
+    symbol: "doge",
   },
   rpcUrls: {
     default: {
-      http: ["https://rpc.shibrpc.com"],
+      http: ["https://rpc.dogechain.dog"],
     },
     public: {
-      http: ["https://rpc.shibrpc.com"],
+      http: ["https://rpc.dogechain.dog"],
     },
   },
   blockExplorers: {
-    default: { name: "Explorer", url: "https://shibariumscan.io" },
+    default: { name: "Explorer", url: "https://explorer.dogechain.dog/" },
   },
 });
 
 export const inscriptionChains = {
   eth: mainnet,
-  bsc,
-  polygon,
-  avalanche,
-  classic,
-  base,
-  arbitrum,
-  zkSync,
-  linea,
-  okc,
-  fantom,
-  opBNB,
-  celo,
-  confluxESpace,
-  gnosis,
-  shibarium,
+  doge,
 };
 
 export type ChainKey = keyof typeof inscriptionChains;
