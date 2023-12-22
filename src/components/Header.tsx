ca0c70c18e934038474c16f73c208ecb95ae016c
mochaza123 <58312722+mochaza123@users.noreply.github.com>
2023-12-22 20:59:28 +0700

    Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useContext } from "react";
 
-import logo from "@/assets/imgs/photo_2023-12-22_20-35-30.jpg";
+import logo from "@/assets/imgs/photo_2023-12-22_20-35-30-removebg-preview.png";
 import ColorModeContext from "@/contexts/colorModeContext";
 
 export default function Header() {
